9510cdbd3ea2f05a09044f8cb1944c58e17b8f28
sebastianhenaosanta <sebastianhenao7@gmail.com>
2020-05-06 03:20:54 -0500

    first commit

---
+++ mulTifilePrograms/firstExample/build/CMakeFiles/multiFiles.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multiFiles.dir/main.cpp.o"
+  "CMakeFiles/multiFiles.dir/my_functions.cpp.o"
+  "multiFiles.pdb"
+  "multiFiles"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/multiFiles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
